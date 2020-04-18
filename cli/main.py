a8722b8021be2ceed02e09a96d1313a66990bccc
nixonbali <nixon.ubud@gmail.com>
2020-04-18 16:03:45 -0400

    first cli commit

---
+++ cli/main.py
@@ -0,0 +1,47 @@
+import sys
+from db.db import Session
+from db.models import Things, Events, Notes
+
+"""
+v0 run w/: $ python3 -m cli.main [args]
+"""
+
+"""
+args:
+
+event [thing_name] [starttime] [endtime=None] [place=None] [duration=None]
+-> triggers newEvent
+-> asks for Note
+-> if yes
+- -> opens note.txt in shell text editor
+- -> upon save/exit -> triggers Event.newNote(session, content=read(note.txt))
+
+note [thing_names]
+-> opens note.txt in shell text editor
+-> upon save/exit -> triggers Note.newNote(session, thing_names=[thing_names], content=read(note.txt)
+"""
+
+def invalid_input(*args):
+    """Handles Invalid Input Argument"""
+    print(f"Invalid Action: {sys.argv[1]}")
+    print("Action must be in [event, note]")
+
+def new_event(*args):
+    """New Event Input"""
+    print('creating new event')
+
+def new_note(*args):
+    """New Note Input"""
+    print('creating new note')
+
+from collections import defaultdict
+switcher = defaultdict(lambda: invalid_input)
+switcher['event'] = new_event
+switcher['note'] = new_note
+
+if __name__ == "__main__":
+    print('whid called')
+    try:
+        switcher[sys.argv[1]](*sys.argv[2:])
+    except IndexError:
+        print('No Actions Taken')
